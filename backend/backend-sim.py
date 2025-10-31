444724d0609c9d5227894a645a0fa2d32c06cc16
April <56981371+aprxl@users.noreply.github.com>
2025-10-31 15:09:55 -0300

    Adicionado arquivos em Python

---
+++ backend/backend-sim.py
@@ -0,0 +1,22 @@
+import socket
+import time
+from .packet_type import Packet, PacketType
+
+HOST = "192.168.4.1"
+PORT = 8080
+
+if __name__ == "__main__":
+    # Criação da conexão TCP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Conexão com a ESP32
+    sock.connect((HOST, PORT))
+
+    initialized = False
+
+    while not initialized:
+        handshake = sock.recv(1024)
+
+        if handshake[0] == PacketType.HANDSHAKE:
+            sock.send(Packet.handshake().data)
+            initialized = True
---
+++ backend/packet_type.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class PacketType(Enum):
+    HANDSHAKE = b"\x01"
+    MOVE = b"\x02"
+    TURN = b"\x03"
+    STATUS = b"\x04"
+
+
+class Packet:
+    def __init__(self, type: PacketType, data: bytes | None) -> None:
+        """Cria um novo pacote de qualquer tipo, com qualquer dados"""
+        self.data = bytes(b"".join([type.value, data or bytes()]))
+
+    @staticmethod
+    def handshake():
+        """Cria um novo pacote de handshake"""
+        return Packet(PacketType.HANDSHAKE, None)
+
+    @staticmethod
+    def move(cm: int):
+        """Cria um novo pacote de movimento"""
+        return Packet(PacketType.MOVE, cm.to_bytes(2, "little"))
